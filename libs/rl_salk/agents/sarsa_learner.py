5987b31c795dddab5e88dcd66adf4b51e2702de7
Dan Butler <daniel.jaffe.butler@gmail.com>
2019-08-06 15:10:42 -0700

    simplify TD agents

--- libs/rl_salk/agents/sarsa_learner.py
+++ libs/rl_salk/agents/sarsa_learner.py
@@ -3,9 +3,6 @@ from rl_salk.agents.td_learner import TDLearner
 
 class SarsaLearner(TDLearner):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def learn(self, prev_state, action, state, reward, done):
         super().learn(prev_state, action, state, reward, done,
                       target_policy='behavior')
