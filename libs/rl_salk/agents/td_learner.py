5e5da302c245679d3032ca3256f4f017f127a836
Dan Butler <daniel.jaffe.butler@gmail.com>
2019-08-06 15:16:58 -0700

    improve TDLearner.py variable names and readability

--- libs/rl_salk/agents/td_learner.py
+++ libs/rl_salk/agents/td_learner.py
@@ -4,14 +4,14 @@ import numpy as np
 
 class TDLearner(object):
 
-    def __init__(self, n_states, n_actions, epsilon=0.1, gamma=0.99,
-                 alpha=0.1):
+    def __init__(self, n_states, n_actions, exploration_prob=0.1,
+                 discount_rate=0.99, learning_rate=0.1):
 
         self.np_random, self.seed = seeding.np_random(None)
 
-        self.epsilon = epsilon
-        self.gamma = gamma
-        self.alpha = alpha
+        self.exploration_prob = exploration_prob
+        self.discount_rate = discount_rate
+        self.learning_rate = learning_rate
 
         self.n_states = n_states
         self.n_actions = n_actions
@@ -25,7 +25,7 @@ class TDLearner(object):
         pass
 
     def sample_action(self, state):
-        if self.np_random.rand() < self.epsilon:
+        if self.np_random.rand() < self.exploration_prob:
             action = self.np_random.randint(0, self.n_actions)  # random action
         else:
             action = np.argmax(self.q[state])  # greedy action
@@ -36,15 +36,17 @@ class TDLearner(object):
 
         (s, a, s_, r) = (prev_state, action, state, reward)
 
+        action_values = self.q[s_, :]
         if target_policy == 'optimal':
-            value = np.max(self.q[s_])
+            value = np.max(action_values)
         elif target_policy == 'behavior':
             a_ = self.sample_action(s_)
-            value = self.q[s_, a_]
+            value = action_values[a_]
         else:
             raise Exception(('target_policy value "{}"'
                              'not recognized').format(target_policy))
 
         # TD update
         self.q[s, a] = (self.q[s, a]
-                        + self.alpha * (r + self.gamma * value - self.q[s, a]))
+                        + self.learning_rate * (r + self.discount_rate * value
+                                                - self.q[s, a]))
